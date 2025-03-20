cf4d1a0ea9a5d43d5b14e951cd7dc9870e770eb4
TJPickering <tjpickering@students.unwsp.edu>
2025-03-19 19:22:54 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,8 +1,81 @@
-# Program #3: Capital Quiz
-# Write a program that creates a dictionary containing the U.S. states as keys, 
-# and their capitals as values.  
-# The program should then randomly quiz the user by displaying the name of a state 
-# and asking the user to enter the state's capital.  
-# The program should count of the number of correct and incorrect responses.  
-# (You could alternatively use another country and provinces, 
-# or countries of the world and capitals).
+#Programmer: Timothy Pickering
+#Date Written: 3/19/2025
+#Title: U.S. States Capitals Quiz
+
+#Required Library
+import random
+
+def quiz_user(state_capitals):
+    correct = 0
+    incorrect = 0
+
+    states = list(state_capitals.keys())  #Get a list of states
+    random.shuffle(states)  #Shuffle them for random order
+
+    for state in states: #Quiz loop
+        user_answer = input(f"What is the capital of {state}? ").strip()
+
+        if user_answer.lower() == state_capitals[state].lower():
+            print("Correct!\n")
+            correct += 1
+        else:
+            print(f"Incorrect! The capital of {state} is {state_capitals[state]}.\n")
+            incorrect += 1
+
+    print(f"You got {correct} correct and {incorrect} incorrect.")
+
+#Dictionary of U.S. states and capitals
+state_capitals = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",}
+
+#Start the quiz
+quiz_user(state_capitals)
